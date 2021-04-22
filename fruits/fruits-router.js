848cbd618fb6c12d4ceeddbdd9cf88e4ef46f2e0
Jason Maurer <jsonmaur@fastmail.com>
2021-04-22 13:04:27 -0600

    Remove inline db config from router

--- fruits/fruits-router.js
+++ fruits/fruits-router.js
@@ -1,15 +1,5 @@
 const express = require("express")
 
-// Temporary until Knex is configured in the project
-const knex = require("knex")
-const db = knex({
-	client: "sqlite3",
-	useNullAsDefault: true,
-	connection: {
-		filename: "./data/produce.db3",
-	},
-})
-
 const router = express.Router()
 
 router.get("/fruits", async (req, res, next) => {
